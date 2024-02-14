adb33979553d1fd8882766c821d90139c73a55dd
vietchinh <1348151+vietchinh@users.noreply.github.com>
2024-02-14 08:07:03 +0100

    fix: actually make an json object

--- .github/parse.sh
+++ .github/parse.sh
@@ -4,6 +4,6 @@ create_matrix_object () {
   (echo [{ \"name\": \"$1\", \"version\": && cat && echo }]) <<< $(curl $2 | grep -zPoh -m 1 -- "\d+.\d+-\d+.\d+\n" | head -1 | tr -d '\n' | tr -d '\0' | awk '{ print "\""$0"\""}') | jq -c .
 }
 
-generated_matrix=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/lxd/lxd.spec") | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/incus/incus.spec")")
-echo $generated_matrix
+merged_array=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/lxd/lxd.spec") | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/incus/incus.spec")")
+generated_matrix=$(echo {\"versions\":$merged_array} | jq -c .)
 echo "matrix=$generated_matrix" >> $GITHUB_OUTPUT
