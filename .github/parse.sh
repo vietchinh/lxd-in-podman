59b8728dbd8acc16e009856addb4abc9a3d27520
vietchinh <1348151+vietchinh@users.noreply.github.com>
2024-03-14 23:14:14 +0100

    fix: disabled incus until fedora 40 release

--- .github/parse.sh
+++ .github/parse.sh
@@ -4,7 +4,8 @@ create_matrix_object () {
   (echo [{ \"name\": \"$1\", \"version\": && cat && echo }]) <<< $(curl $2 | grep -zPoh -m 1 -- '\d+.\d+-\d+.\d+\n|\d+.\d+.\d+-\d+.\d+\n' | head -1 | tr -d '\n' | tr -d '\0' | awk '{ print "\""$0"\""}') | jq -c .
 }
 
-merged_array=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/lxd/lxd.spec") | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/incus/incus.spec")")
+#  | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/incus/incus.spec")")
+merged_array=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/lxd/lxd.spec")
 generated_matrix=$(echo {\"packages\":$merged_array} | jq -c .)
 echo $generated_matrix
 echo "matrix=$generated_matrix" >> $GITHUB_OUTPUT
