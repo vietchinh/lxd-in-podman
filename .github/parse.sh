dc826d8cbee90a335d56f9257f9f0ca6d0c514e9
Vietchinh Pham <1348151+vietchinh@users.noreply.github.com>
2024-03-22 19:27:01 +0100

    Fix/consistent matrix generation (#9)
    * Revert "Reapply "fix: disabled incus until fedora 40 release""
    
    This reverts commit ee6ee8abcf8145a8d347b4b3dc6e38122d05a53a.
    
    * feat: use .tito link of the github repo for actual versioning

--- .github/parse.sh
+++ .github/parse.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 create_matrix_object () {
-  (echo [{ \"name\": \"$1\", \"version\": && cat && echo }]) <<< $(curl $2 | grep -zPoh -m 1 -- '\d+.\d+-\d+.\d+\n|\d+.\d+.\d+-\d+.\d+\n' | head -1 | tr -d '\n' | tr -d '\0' | awk '{ print "\""$0"\""}') | jq -c .
+  (echo [{ \"name\": \"$1\", \"version\": && cat && echo }]) <<< $(curl $2 | grep -zPoh -m 1 -- '\d+[.-]*' | tr -d '\n' | tr -d '\0' | awk '{ print "\""$0"\""}') | jq -c .
 }
 
-#  | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/incus/incus.spec")")
-merged_array=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/lxd/lxd.spec"))
+merged_array=$(echo $(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/51bd8355fc7216837323ef2d0babbe95bd65338d/.tito/packages/lxd") | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/51bd8355fc7216837323ef2d0babbe95bd65338d/.tito/packages/incus")")
 generated_matrix=$(echo {\"packages\":$merged_array} | jq -c .)
 echo $generated_matrix
 echo "matrix=$generated_matrix" >> $GITHUB_OUTPUT
