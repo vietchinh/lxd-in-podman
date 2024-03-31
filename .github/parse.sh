226a02ed980c586ef459597d2e8ab0f7f280ee88
vietchinh <1348151+vietchinh@users.noreply.github.com>
2024-03-31 06:51:06 +0200

    fix: version of lxd and incus is mapped to static hash of the repo

--- .github/parse.sh
+++ .github/parse.sh
@@ -4,7 +4,7 @@ create_matrix_object () {
   (echo ["{" \"name\": \""$1"\", \"version\": && cat && echo "}"]) <<< "$(curl "$2" | grep -zPoh -m 1 -- '\d+[.-]*' | tr -d '\n' | tr -d '\0' | awk '{ print "\""$0"\""}')" | jq -c .
 }
 
-merged_array="$(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/51bd8355fc7216837323ef2d0babbe95bd65338d/.tito/packages/lxd" | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/51bd8355fc7216837323ef2d0babbe95bd65338d/.tito/packages/incus")")"
+merged_array="$(create_matrix_object "lxd" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/.tito/packages/lxd" | jq ". + $(create_matrix_object "incus" "https://raw.githubusercontent.com/ganto/copr-lxc4/master/.tito/packages/incus")")"
 generated_matrix=$(echo "{" \"packages\":"$merged_array""}" | jq -c .)
 echo "$generated_matrix"
 echo "matrix=$generated_matrix" >> "$GITHUB_OUTPUT"
